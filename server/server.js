a49d57d20333b5831de825488b7989109633f3e3
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-24 15:39:32 +0530

    learning to create a api which handle the get and post requst from the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const app = require('./src/app')
 
+
+
+// to start the server 
 app.listen(3000,()=>{
     console.log("server is running on port");
     
--- server/src/app.js
+++ server/src/app.js
@@ -2,4 +2,29 @@ const express = require('express')
 
 const app =express()
 
+const notes=[];
+
+// middleware  
+app.use(express.json())
+
+
+// post request to handle the post request from the client or postman
+app.post('/notes',(req,res)=>{
+    console.log(req.body)
+    notes.push(req.body);
+    res.status(201).json({
+        message:"notes created  succesfully"
+    })
+})
+
+// get method ,it is use to handle the get request
+app.get('/notes',(req,res)=>{
+    
+    res.status(200).json({
+        message:"notes fetched Successfuly",
+        notes:notes
+    })
+})
+
+
 module.exports=app
