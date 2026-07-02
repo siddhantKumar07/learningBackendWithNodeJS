1ebfb1c5c55f33b51ce820cd7b876e2736e0e5bf
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-02 12:39:01 +0530

    learning with project

--- LearningWithProject/src/app.js
+++ LearningWithProject/src/app.js
@@ -5,6 +5,28 @@ const app = express();
 
 app.use(express.json())
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
+app.get("/user",(req,res)=>{
+    res.status(200).json({
+        message:"fetched successfully"
+    })
 })
+app.post("/user",(req,res)=>{
+    console.log(req.body);
+    res.status(200).json({
+        message:"received successfully"
+    })
+})
+app.delete("/user",(req,res)=>{
+    res.status(200).json({
+        message:"deleted successfully"
+    })
+})
+app.patch("/user",(req,res)=>{
+    console.log(req.body);
+    res.status(200).json({
+        message:"updated successfully"
+    })
+})
+  
+
+module.exports = app;
