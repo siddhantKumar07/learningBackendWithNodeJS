bedc1b8d5e93697b031043d44624f616ea590bfb
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-02 13:11:10 +0530

    learning with project

--- LearningWithProject/src/app.js
+++ LearningWithProject/src/app.js
@@ -5,13 +5,19 @@ const app = express();
 
 app.use(express.json())
 
+const storage=[];
 app.get("/user",(req,res)=>{
+    // console.log(req.params.userid);
+    // console.log(req.params.name);
+    console.log(req.query);
     res.status(200).json({
-        message:"fetched successfully"
+        message:"fetched successfully",
+        storage:storage
     })
 })
 app.post("/user",(req,res)=>{
     console.log(req.body);
+    storage.push(req.body);
     res.status(200).json({
         message:"received successfully"
     })
