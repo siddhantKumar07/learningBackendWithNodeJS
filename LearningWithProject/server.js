ea27d1886dacd21297229f86b7a5870bee9c90f8
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-07 12:49:38 +0530

    adding middleware to parse the Json to js obj

--- LearningWithProject/server.js
+++ LearningWithProject/server.js
@@ -1,6 +1,10 @@
 const app = require("./src/app")
 const connectDB = require("./src/config/db");
-connectDB()
+try {
+    connectDB();
+} catch (error) {
+    console.error("Error connecting to database:", error);
+}
 app.listen(3000, () => {
   
     console.log("server is running on port 3000")
--- LearningWithProject/src/app.js
+++ LearningWithProject/src/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const userModel = require("./model/user");
 const app = express();
 
-app.use(express.json())
+app.use(express.json())//It parses incoming request body (JSON data) and converts it into a JavaScript object. it is works for all the routes in the application, allowing them to access the request body data as a javascript object through req.body. It is important to use this middleware before defining any routes that expect to receive JSON data in the request body, as it ensures that the data is properly parsed and available for use in the route handlers.
+
 const {userAuth,adminAuth} = require("./middleware/auth")
 
 // middleware is used to handle the unauthorized access to the routes
