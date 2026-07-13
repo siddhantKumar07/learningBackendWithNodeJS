488e367cd549f42fdb556b9af4836ee6a6264845
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-13 21:52:52 +0530

    adding some more logics into the connections api

--- LearningWithProject/src/routes/userRouter.js
+++ LearningWithProject/src/routes/userRouter.js
@@ -4,31 +4,32 @@ const userRouter = express.Router();
 const ConnectionRequestModel = require("../model/connectionRequest");
 const userModel = require("../model/user");
 
+const userSafeData = [
+  "firstName",
+  "lastName",
+  "photoUrl",
+  "age",
+  "gender",
+  "skills",
+  "about",
+];
 // it will return all the connection which is accepted
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const { _id } = loggedInUser;
     const connections = await ConnectionRequestModel.find({
-     $or:[
+      $or: [
         {
-            senderId:_id,
-            status:"accepted"
+          senderId: _id,
+          status: "accepted",
         },
         {
-            receiverId:_id,
-            status:"accepted"
-        }
-     ]
-    }).populate("senderId", [
-      "firstName",
-      "lastName",
-      "photoUrl",
-      "age",
-      "gender",
-      "skills",
-      "about",
-    ]);
+          receiverId: _id,
+          status: "accepted",
+        },
+      ],
+    }).populate("senderId", userSafeData);
 
     if (connections.length === 0) {
       return res.status(404).json({
@@ -56,15 +57,7 @@ userRouter.get("/user/pendingRequest", userAuth, async (req, res) => {
     const pendingRequest = await ConnectionRequestModel.find({
       receiverId: _id,
       status: "interested",
-    }).populate("senderId", [
-      "firstName",
-      "lastName",
-      "photoUrl",
-      "age",
-      "gender",
-      "skills",
-      "about",
-    ]);
+    }).populate("senderId",userSafeData);
     //. populate is used to get the data from the senderId which is a reference to the user model and we are getting the firstName,lastName,photoUrl,age,gender,skills,about from the user model.
 
     if (pendingRequest.length === 0) {
@@ -84,6 +77,7 @@ userRouter.get("/user/pendingRequest", userAuth, async (req, res) => {
 });
 
 
+// for to get all the user data in the feed of the logged in user
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const storage = await userModel.find();
@@ -99,7 +93,6 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
       about: user.about,
     }));
 
-
     if (users.length === 0) {
       return res.status(404).json({
         message: "no user found",
@@ -114,7 +107,7 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
 
     res.status(200).json({
       message: "fetched successfully",
-        users: users,
+      users: users,
     });
   } catch (error) {
     res.status(500).json({
