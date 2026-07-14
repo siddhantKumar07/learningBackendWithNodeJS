44aa3de57be3e5139d7811259b1ca631423bd05a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-14 23:12:14 +0530

    adding toast

--- LearningWithProjectFrontend/src/pages/Login.jsx
+++ LearningWithProjectFrontend/src/pages/Login.jsx
@@ -1,23 +1,50 @@
 import React, { useState } from "react";
-
+import { ToastContainer, Bounce, toast } from "react-toastify";
 const Login = () => {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState()
-    
-    const handleLogin = async() => {
-        console.log(email + ' ' + password )
-  const response = await fetch('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ emailId:email, password:password })
-        })
-        const data = await response.json()
-        console.log(data)
-        setEmail('')
-        setPassword('')
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleLogin = async () => {
+    console.log(email + " " + password);
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ emailId: email, password: password }),
+    });
+    const data = await response.json();
+    console.log(data.message);
+    setEmail("");
+    setPassword("");
+
+    if (data.message === "login successful") {
+      toast.success("Login succesfull!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
+    else if(data.message === "invalid Credentials"){
+toast.warn('Invalid credentials!', {
+position: "top-right",
+autoClose: 2000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+transition: Bounce,
+});
+    }
+  };
 
   return (
     <div className="flex justify-center my-16">
@@ -29,8 +56,8 @@ const Login = () => {
           className="input font-semibold outline-none text-lg"
           placeholder="Email"
           value={email}
-          onChange={(e)=>{
-            setEmail(e.target.value)
+          onChange={(e) => {
+            setEmail(e.target.value);
           }}
         />
 
@@ -40,17 +67,31 @@ const Login = () => {
           className="input font-semibold outline-none text-lg"
           placeholder="Password"
           value={password}
-          onChange={(e)=>{
-            setPassword(e.target.value)
+          onChange={(e) => {
+            setPassword(e.target.value);
           }}
         />
 
-        <button className="btn btn-neutral mt-4 font-bold text-xl rounded-box"
-        onClick={handleLogin}
+        <button
+          className="btn btn-neutral mt-4 font-bold text-xl rounded-box"
+          onClick={handleLogin}
         >
           Login
         </button>
       </fieldset>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </div>
   );
 };
