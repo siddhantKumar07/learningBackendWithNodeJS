517428c4e32daeec24b4955aade4c74d28cdc1c7
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-18 14:56:47 +0530

    storing messages on database

---
+++ LearningWithProject/src/routes/messageRoute.js
@@ -0,0 +1,29 @@
+const { userAuth } = require("../middleware/auth");
+const chatModel = require("../model/chat");
+const express = require("express");
+const messageRouter = express.Router();
+
+messageRouter.get("/messages/:senderId/:receiverId",userAuth,async(req,res)=>{
+    
+try{
+const {senderId,receiverId} = req.params;
+const chat = await chatModel.findOne({participants:{$all:[senderId,receiverId]}}).populate("messages.senderId","firstName lastName photoUrl");
+
+if(!chat){
+    return res.status(404).json({
+        message:"No chat found between the users"
+    })
+}
+return res.status(200).json({
+    message:"chat fetched successfully",
+    chat:chat
+})
+
+}catch(error){
+    res.status(500).json({
+        message:error.message
+    })
+
+}
+
+})
--- LearningWithProject/src/utils/socket.js
+++ LearningWithProject/src/utils/socket.js
@@ -1,5 +1,11 @@
 const socket = require("socket.io");
 const cors = require("cors");
+const crypto = require("crypto");
+const { createRequire } = require("module");
+const chatModel = require("../model/chat");
+const createRoomId=(senderId,receiverId)=>{
+    return crypto.createHash("sha256").update([senderId,receiverId].sort().join("_")).digest("hex");
+}
 
 const intializeSocket = (server)=>{
  
@@ -11,16 +17,37 @@ cors:{
 io.on("connection",(socket)=>{
    
     socket.on("joinChat",({senderId,receiverId})=>{
-        const roomId = [senderId,receiverId].sort().join("_");
+        const roomId = createRoomId(senderId,receiverId);
         console.log("user joined room:"+roomId);
         socket.join(roomId);
     })
 
-    socket.on("sendMessage",({firstName,senderId,receiverId,receiverName,message})=>{
-        const roomId = [senderId,receiverId].sort().join("_");
+    socket.on("sendMessage",async({firstName,senderId,receiverId,receiverName,message})=>{
+        const roomId = createRoomId(senderId,receiverId);
         console.log("message sent to room:"+roomId);
         console.log("message:"+message+" from:"+firstName );
-        io.to(roomId).emit("receiveMessage",{firstName,receiverName,message}); 
+       
+        try{
+   const user = await chatModel.findOne({participants:{$all:[senderId,receiverId]}});
+   if(user){
+    user.messages.push({senderId,text:message});
+    user.save();
+   }else{
+    const newChat = await new chatModel({
+        participants:[senderId,receiverId],
+        messages:[{senderId,text:message}]
+    })
+    await newChat.save();
+        io.to(roomId).emit("receiveMessage",{firstName,receiverName,message,timestamp:new Date().toISOString()}); 
+
+   }
+
+        }catch(err){
+            console.log(err);
+        }
+
+
+
         
     })
 
