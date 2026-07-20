2ab8b286562243834cb6da3f36e23e44b4f17d1a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-20 22:30:23 +0530

    creating the schema of database

--- contentCreationProj/backend/src/models/post.model.js
+++ contentCreationProj/backend/src/models/post.model.js
@@ -1,7 +1,17 @@
 const mongooes = require("mongoose");
 
 const postSchema = new mongooes.Schema({
-
+imageUrl:{
+    type:String,
+    required:true,
+    trim:true
+},
+caption:{
+    type:String,
+    required:true,
+    trim:true,
+    maxLength:300
+},
 })
 const postModel=mongooes.model("post",postSchema);
 
