0dd3f5e6e9f6da6286c6b8ae11cf867811b06466
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-21 10:52:30 +0530

    Remove node_modules from repository

--- contentCreationProj/backend/src/app.js
+++ contentCreationProj/backend/src/app.js
@@ -1,6 +1,41 @@
 const express = require("express");
-
+const axios = require("axios");
+const multer = require("multer");
 const app = express();
+app.use(express.json());// to parse the incoming request as js object
+
+const upload = multer({//multer is used to upload the file
+    storage:multer.memoryStorage(),
+    limits:{
+        fileSize:5*1024*1024 // 5mb
+    }
+})
+app.post("/createPost",upload.single("image"),async(req,res)=>{
+    try{
+        const {caption} = req.body;
+        if(!caption){
+            return res.status(400).json({
+                message:"Caption is required"
+            })
+        }
+        const image = req.file;
+        if(!image){
+            return res.status(400).json({
+                message:"Image is required"
+            })
+        }
+        console.log(image);
+        console.log(caption);
+        return res.status(201).json({
+            message:"Post created successfully",
+        })
+      
+    }catch(err){
+      res.status(500).json({
+        message:err.message,
+      })
+    }
+})
 
 
 
