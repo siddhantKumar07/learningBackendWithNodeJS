3d757b6cde6f743d8b651e5e5cc3ce3194ccebdb
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-22 21:22:28 +0530

    added cookie parser to read the cookies

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const authRouter = require("./src/routes/Auth.routes");
+const cookieParser = require("cookie-parser");
 const app = express();
-app.use(express.json());
+app.use(express.json())
+app.use(cookieParser());
 app.use("/api/auth",authRouter);
 
 
