8e60b536b426dfd377affd3c4b48c40ebf49cc47
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-23 11:31:43 +0530

    learning the workign of jwt

--- Authentication/src/routes/post.routes.js
+++ Authentication/src/routes/post.routes.js
@@ -12,10 +12,14 @@ postRouter.post("/create",async (req, res) => {
         })
     }
     const data = await jwt.verify(token,process.env.JWT_SECRET)
-      console.log(data);
+    // jwt token has three parts separated by dots (header, payload, signature). header is the algorithm which is used to sign the token 
+    // payload is the data which is store in the token 
+    // signature is created by combining the header,payload and secret key using the algorithm specified in the header.
+    // for the verification it create a new signature with the header payload and the jwt secret and compare the new signature with the old one 
+    
     if(!data){
         return res.status(401).json({
-            message:"Unauthorized",
+            message:"Unauthorized Access",
         })
     }
 
