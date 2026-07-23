18876825be129f0b8721b77be97bf524cef40be3
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-23 17:18:32 +0530

    adding some extra validation

--- ProjectSpotifyBackend/src/controllers/authController.js
+++ ProjectSpotifyBackend/src/controllers/authController.js
@@ -3,6 +3,17 @@ const userModel = require("../models/user.model");
 const registerAuthController = async (req ,res)=>{
 const {email,password,username,role} = req.body;
 try{
+    const existUser = await userModel.findOne({
+        $or:[
+            {email:email},
+            {username:username}
+        ]
+    })
+    if(existUser){
+        return res.status(400).json({
+            message:"User already exists"
+        })
+    }
 const hashedPass = await bcrypt.hash(password,10);
 const user = await userModel.create({
     username:username,
@@ -11,13 +22,16 @@ const user = await userModel.create({
     role:role
 });
 
-res.status(201).json({
+return res.status(201).json({
     message:"User created successfully",
     user:user
 })
 
 }catch(error){
-
+    return res.status(500).json({
+        message:"Error creating user",
+        error:error.message
+    })
 }
 }
 module.exports = registerAuthController;
--- ProjectSpotifyBackend/src/middlewares/authMiddleware.js
+++ ProjectSpotifyBackend/src/middlewares/authMiddleware.js
@@ -5,22 +5,22 @@ const registerAuthMiddleware= (req,res,next)=>{
         return res.status(400).json({message:"All fields are required"});
     }
     if(!username.length>7&&!username.length<21){
-        res.status(400).json({
+      return  res.status(400).json({
             message:"username must be at least  8 characters long and less than 21 characters long"
         })
     }
     if(!validator.isEmail(email)){
-        res.status(400).json({
+        return res.status(400).json({
             message:"Invalid email"
         })
     }
     if(!validator.isStrongPassword(password)){
-        res.status(400).json({
+        return res.status(400).json({
             message:"Password is not strong enough"
         })
     }
     if(!["user","artist"].includes(role)){
-        res.status(400).json({
+        return res.status(400).json({
             message:"Role must be either user or artist"
         })
     }
