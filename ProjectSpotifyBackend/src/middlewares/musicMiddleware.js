640cc6d6b41918e1be68ab9be80916bfee0fed73
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-26 23:54:56 +0530

    making middleware

--- ProjectSpotifyBackend/src/middlewares/musicMiddleware.js
+++ ProjectSpotifyBackend/src/middlewares/musicMiddleware.js
@@ -2,7 +2,17 @@ const jwt = require("jsonwebtoken")
 const createMusicMiddleware = (req,res,next)=>{
     const cookies = req.cookies;
     const {token} = cookies;
-    console.log("token",token)
-    console.log("role",token.role)
+  if(!token){
+    return res.status(401).json({
+        message:"Unauthorized"
+    })
+  }
+  if(role !== "artist"){
+    return res.status(403).json({
+        message:"you are not authorized to create music"
+    })
+  }
+  
+
 }
 module.exports = createMusicMiddleware;
