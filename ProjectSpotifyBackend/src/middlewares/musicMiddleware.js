c281ae774e049c2d63faa45e51a113c2981f6d2c
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-27 21:22:39 +0530

    doing some debugging

--- ProjectSpotifyBackend/src/middlewares/musicMiddleware.js
+++ ProjectSpotifyBackend/src/middlewares/musicMiddleware.js
@@ -10,8 +10,6 @@ const createMusicMiddleware = (req, res, next) => {
       message: "Unauthorized"
     });
   }
-  
-
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   console.log("decoded", decoded);
 
