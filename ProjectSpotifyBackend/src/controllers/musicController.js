0ce696deb554adfce2f7430e55bfef7add864270
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-28 20:38:11 +0530

    adding controller

--- ProjectSpotifyBackend/src/controllers/musicController.js
+++ ProjectSpotifyBackend/src/controllers/musicController.js
@@ -5,14 +5,10 @@ const uploadImage = require("../service/imageUpload.service")
 const musicModel = require("../models/music.model")
 
 const CreateMusicController =async (req,res)=>{
-
 try{
 const user = req.user;
 const {title}= req.body
 const musicFile = req.file;
-console.log("user",user);
-console.log("title",title);
-console.log("musicFile",musicFile);
     const response = await uploadImage(musicFile.buffer,musicFile.originalname);
     if(response.error){
         return res.status(500).json({
@@ -27,19 +23,9 @@ const newMusic= await musicModel.create({
     artist:user.id
 }) 
 
-const musicWithArtist = await musicModel
-  .findById(newMusic._id)
-  .populate("artist",["username"]);
-
-console.log(musicWithArtist.artist);
-console.log("user",musicWithArtist);
-
-
-
-
-
-
-
+// const musicWithArtist = await musicModel
+//   .findById(newMusic._id)
+//   .populate("artist",["username"]);
 
 return res.status(200).json({
         message:"Music created successfully"
