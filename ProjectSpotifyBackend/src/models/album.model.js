8208788b13b8a3a80a5027d07d2298d49fa0c824
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-29 10:08:26 +0530

    creating a album model

---
+++ ProjectSpotifyBackend/src/models/album.model.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose");
+
+const albumSchema  = new mongoose.Schema({
+   title:{
+    type:String,
+    required:true,
+    trim:true
+   },
+   musics:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"music",
+    required:true
+   }],
+   artist:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user",
+    required:true
+   }
+})
+const albumModel = mongoose.model("album",albumSchema);
+module.exports = albumModel;
