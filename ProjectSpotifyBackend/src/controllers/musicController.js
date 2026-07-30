ea94f9aedcf6334952db7a816ee86e125cbfa302
siddhantKumar07 <siddhantk80044@gmail.com>
2026-07-30 15:45:19 +0530

    fetching music

--- ProjectSpotifyBackend/src/controllers/musicController.js
+++ ProjectSpotifyBackend/src/controllers/musicController.js
@@ -44,6 +44,7 @@ return res.status(500).json({
 const getAllMusicController = async (req, res) => {
 const user = req.user;
 try{
+  
     const allMusic = await musicModel.find().populate("artist",["username"]);
     return res.status(200).json({
         message:"All music fetched successfully",
