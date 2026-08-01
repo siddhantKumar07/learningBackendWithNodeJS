6b2c1d6163c832acb541d3796b68bff8845bb6d7
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-01 19:02:47 +0530

    adding bcrypt

--- project_Banking_system/src/model/user.model.js
+++ project_Banking_system/src/model/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require("bcrypt")
 const userSchema =new  mongoose.Schema({
     name:{
         type:String,
