998eaadeac0e29eed6096cb5303cdf2f530982ce
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-01 21:55:53 +0530

    adding routes

---
+++ project_Banking_system/src/middleware/auth.middleware.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+const authMiddleware = (req,res,next)=>{
+    const {token } = req.cookies;
+    if(!token){
+        return res.status(401).json({
+            success:false,
+            message:"Please login to access this resource"
+        })
+    }
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+}
--- project_Banking_system/src/routes/auth.route.js
+++ project_Banking_system/src/routes/auth.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const authRouter = express.Router();
 
+authRouter.post("/register")
 
 
 
