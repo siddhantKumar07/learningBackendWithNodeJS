e34b4dc9726b51c7c143cc6920fdc02965e69d29
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-01 21:50:49 +0530

    adding routes

--- project_Banking_system/src/app.js
+++ project_Banking_system/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
+const authRouter = require('./routes/auth.route');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
+// auth router 
+app.use("/api/auth",authRouter);
 
 module.exports = app;
