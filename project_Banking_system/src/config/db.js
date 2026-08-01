319460c48873957b48a02b2bee5288a5a9906d27
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-01 18:24:16 +0530

    created the model

--- project_Banking_system/src/config/db.js
+++ project_Banking_system/src/config/db.js
@@ -6,6 +6,7 @@ const connectDB = async ()=>{
    console.log("Connected to MongoDB successfully");
     }catch(error){
         console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // Exit the process with an error code
     }
 }
 module.exports = connectDB;
---
+++ project_Banking_system/src/model/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const userSchema =new  mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter your name"],
+        maxLength:[30,"Name cannot exceed 30 characters"],
+        minLength:[4,"Name should have more than 4 characters"],
+        tolowercase:true
+    },
+   email:{
+    type:String,
+    required:[true,"Please enter your email"],
+    unique:[true,"Email already exists"],
+   validate:{
+    validator:validator.isEmail,
+    message:"Please enter a valid email"
+   }
+   },
+   password:{
+    type:String,
+    required:[true,"password is required"],
+    validate:{
+        validator = validator.isStrongPassword,
+        message:"password should be atleast 8 character long ,should be one special symbol,  number and one uppercase letter"
+    },
+    select:false 
+   }
+},{
+    timestamps:true
+})
