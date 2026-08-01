523f202a37eedb693b4ae1ef58201620d428ce66
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-01 21:58:15 +0530

    adding routes

--- project_Banking_system/src/middleware/auth.middleware.js
+++ project_Banking_system/src/middleware/auth.middleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
-const authMiddleware = (req,res,next)=>{
+const userModel = require("../model/user.model");
+const authMiddleware =async (req,res,next)=>{
     const {token } = req.cookies;
     if(!token){
         return res.status(401).json({
@@ -7,7 +8,14 @@ const authMiddleware = (req,res,next)=>{
             message:"Please login to access this resource"
         })
     }
-    const decoded = jwt.verify(token,process.env.JWT_SECRET);
-    req.user = decoded;
+    const decoded =await jwt.verify(token,process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded._id);
+    if(!user){
+        return res.status(401).json({
+            success:false,
+            message:"User not found"
+        })  
+    }
+    req.user = user;
     next();
 }
