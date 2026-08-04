5d861f1aeb62a76b6db87beb8db016d9bc6aca6f
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-04 23:27:33 +0530

    creating a api for handling account creation

--- project_Banking_system/src/routes/acount.route.js
+++ project_Banking_system/src/routes/acount.route.js
@@ -3,6 +3,8 @@ const { authMiddleware } = require("../middleware/auth.middleware");
 
 const accountRouter = express.Router();
 
+// @route POST request to /api/account
+// @desc create a new account for the authenticated user
 accountRouter.post("/",authMiddleware)
 
 module.exports = accountRouter;
