53f137b77142b46a15a4aa9ba937ede0d9f59c35
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-06 20:53:00 +0530

    adding transaction schema to store the transaction

--- project_Banking_system/src/model/transaction.model.js
+++ project_Banking_system/src/model/transaction.model.js
@@ -3,7 +3,7 @@ const transactionSchema = new mongoose.Schema({
     fromAccount:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"Account",
-        required:[true,"transaction must have a from account"]
+        required:[true,"transaction must have a from account"],
         index:true
     },
     toAccount:{
@@ -11,7 +11,30 @@ const transactionSchema = new mongoose.Schema({
         ref:"Account",
         required:[true,"transaction must have a to account"],
         index:true
+    },
+    status:{
+        type:String,
+        enum:{
+            values:["pending","success","failed","reversed"],
+            message:"status must be either pending,success,failed or reversed"
+        },
+        required:[true,"transaction must have a status"],
+        default:"pending"
+    },
+    amount:{
+        type:Number,
+        required:[true,"transaction must have an amount to process"],
+        min:[1,"transaction amount must be greater than 0"] 
+    },
+    idempotencyKey:{
+     type:String,
+     required:[true,"transaction must have an idempotency key"],
+     unique:true,
+     index:true
     }
+},
+{
+    timestamps:true
 })
 const transactionModel = mongoose.model("Transaction",transactionSchema)
 module.exports = transactionModel
