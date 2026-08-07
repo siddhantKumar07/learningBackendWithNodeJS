b2035120fe28a2698cbecf92406f7f3870c5306e
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-07 21:07:55 +0530

    adding a controller

--- project_Banking_system/src/controller/transaction.controller.js
+++ project_Banking_system/src/controller/transaction.controller.js
@@ -120,7 +120,14 @@ const createTransaction = async (req, res) => {
 }
 
 const createInitialFundsTransactionController = async(req,res)=>{
-    
+    const {toAccount,amount,idempotencyKey} = req.body;
+    if(!toAccount || !amount || !idempotencyKey){
+    return res.status(400).json({
+        success:false,
+        message:"Please provide toAccount,amount and idempotencyKey"
+    })
+
+    }
 }
 
 module.exports=createTransaction
