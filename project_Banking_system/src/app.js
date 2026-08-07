99f63bcb979acd28938e46808b316ee564b10d76
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-07 10:00:55 +0530

    handling the transaction while creating a api

--- project_Banking_system/src/app.js
+++ project_Banking_system/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require("cookie-parser")
 const authRouter = require('./routes/auth.route');
 const accountRouter = require('./routes/acount.route');
+const transactionRouter = require("./routes/transaction.route")
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -13,4 +14,7 @@ app.use("/api/auth",authRouter);
 
 // account router
 app.use("/api/account",accountRouter);
+
+// transaction router
+app.use("/api/transaction",transactionRouter);
 module.exports = app;
