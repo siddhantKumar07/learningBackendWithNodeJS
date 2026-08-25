bbe761d2c757b3a45aaa3236fdf02964db8dd131
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-25 23:27:57 +0530

    connnecting the database

---
+++ ProjectInterview/src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const connectDb= async()=>{
+  try{
+ await mongoose.connect(process.env.MONGO_URI,{dbName:"InterviewProject"})
+ console.log("Database connected Successfully")
+  }catch(error){
+    console.log(error.message)
+  }
+}
+
+module.exports=connectDb
