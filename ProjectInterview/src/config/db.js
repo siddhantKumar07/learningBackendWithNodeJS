a217679f518e50a8d0d266ee245f0cfb520168fb
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-26 23:21:43 +0530

    adding some logic

--- ProjectInterview/src/config/db.js
+++ ProjectInterview/src/config/db.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 
 const connectDb= async()=>{
   try{
+
  await mongoose.connect(process.env.MONGO_URI,{dbName:"InterviewProject"})
  console.log("Database connected Successfully")
+ 
   }catch(error){
     console.log(error.message)
   }
