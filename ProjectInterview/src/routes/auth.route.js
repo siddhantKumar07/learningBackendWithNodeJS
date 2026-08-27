afd8eeb9548a7e8a41eaa3b3c7c765f3ee180549
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-27 23:15:22 +0530

    adding the auth router

--- ProjectInterview/src/routes/auth.route.js
+++ ProjectInterview/src/routes/auth.route.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const authRouter = express.Router();
 
-
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
+authRouter.post("/register")
 
 
 
