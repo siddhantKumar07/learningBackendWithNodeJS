91b35f3fa31260bac8fda609e2416f1db98e335f
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-27 22:56:53 +0530

    creating the model to store the data

--- ProjectInterview/src/model/user.model.js
+++ ProjectInterview/src/model/user.model.js
@@ -1,15 +1,29 @@
 const mongoose = require("mongoose")
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema({
   userName:{
     type:String,
     required:true,
-    unique:[true,"UserName is already taken"]
+    unique:[true,"UserName is already taken"],
   },
   email:{
  type:String,
  required:true,
- unique:[true,"Email is already taken"]
+ unique:[true,"Email is already taken"],
+ validator:{
+    validate:validator.isEmail,
+    message:"Please enter a valid email"
+ }
+
+  },
+  password:{
+    type:String,
+    required:true,
+    minlength:[6,"Password must be at least 6 characters long"],
+    validator:{
+        validate:validator.isStrongPassword,
+        message:"Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    }
   }
 })
 const userModel = mongoose.model("user",userSchema)
