bc369b2967589f57891714e0cf02601686d73e91
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-27 22:51:38 +0530

    creating the model

--- ProjectInterview/src/model/user.model.js
+++ ProjectInterview/src/model/user.model.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-
+  userName:{
+    type:String,
+    required:true,
+    unique:[true,"UserName is already taken"]
+  },
+  email:{
+ type:String,
+ required:true,
+ unique:[true,"Email is already taken"]
+  }
 })
 const userModel = mongoose.model("user",userSchema)
 module.exports = userModel
