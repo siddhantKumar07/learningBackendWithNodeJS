0ffd63ded92c9ec5951bd9560e75b20f0be7b161
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-28 09:02:09 +0530

    created the register api

--- ProjectInterview/src/controllers/authController.js
+++ ProjectInterview/src/controllers/authController.js
@@ -1,3 +1,24 @@
+const userModel = require("../model/user.model");
 const registerController = (req,res)=>{
+   try{
+     const {username,email,password} = req.body;
+
+
+    const user = new userModel({
+        userName:username,
+        email:email,
+        password:password
+    });
+    const token = user.jwtToken();
     
+     res.cookies("token",token,{
+        httpOnly:true,
+     })
+
+    user.save()
+    return res.status(201).json({user:user,message:"User registered successfully"});
+   }catch(err){
+    return res.status(500).json({message:"Internal server error"});
+   }
+
 }
