7708eabed41ee3e5c7f04e645549d0c513806d90
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-31 19:58:52 +0530

    adding cookie-parser

--- ProjectInterview/src/app.js
+++ ProjectInterview/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authRouter = require("./routes/auth.route");
+const cookieParser = require("cookie-parser")
 const app = express();
+app.use(cookieParser());// this will parse the cookies from the request and make them available in req.cookies
 app.use(express.json());
 app.use("/api/auth",authRouter);
 
