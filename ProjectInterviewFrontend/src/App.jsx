5b22272167ee0cf6a64122eaf03f8be819f60cdf
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-01 22:59:01 +0530

    setting the router

--- ProjectInterviewFrontend/src/App.jsx
+++ ProjectInterviewFrontend/src/App.jsx
@@ -1,6 +1,8 @@
+import {router} from "./app.routes.jsx";
+import { RouterProvider } from "react-router";
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
