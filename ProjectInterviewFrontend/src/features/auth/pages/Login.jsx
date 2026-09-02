08753af4aca9c5c1cbe3e7bd4e0b04e2e58fc718
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-02 19:57:21 +0530

    creating the login form

--- ProjectInterviewFrontend/src/features/auth/pages/Login.jsx
+++ ProjectInterviewFrontend/src/features/auth/pages/Login.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='flex justify-center items-center h-screen w-full bg-gray-100'>
+      <form className='h-[70%] w-[60%]'>
+        <h1>Login</h1>
+        <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        </div>
+        <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        </div>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+
+    </main>
   )
 }
 
