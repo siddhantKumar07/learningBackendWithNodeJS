6ae7847ad10ae3cd030b8fb4b7d804595e66c3fb
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-04 12:36:01 +0530

    using the hook folder to manage the api and state

--- ProjectInterviewFrontend/src/features/auth/hooks/useAuth.js
+++ ProjectInterviewFrontend/src/features/auth/hooks/useAuth.js
@@ -5,5 +5,44 @@ import { RegisterApi,LoginApi,LogoutApi,GetProfile } from "../services/auth.api.
 export const useAuth=()=>{
 const context = useContext(AuthProvider)
     const {user,setUser,loading,setLoading} = context;
-    
+
+    const handleLogin =async({email,password})=>{
+        setLoading(true)
+        try{
+            const data = await LoginApi({email,password})
+            setUser(data?.user)
+            setLoading(false)
+        }
+        catch(err){
+            setLoading(false)
+            throw err
+        }
+    }
+
+    const handleRegister = async ({username,email,password})=>{
+        setLoading(true);
+        try{
+          const data = await RegisterApi({username,email,password})
+          setUser(data?.user);
+          setLoading(false)
+        }catch(error){
+            setLoading(false)
+            throw error
+        }
+    }
+
+    const handleLogout= async()=>{
+        setLoading(true);
+         const data = await LogoutApi();
+         setUser(null);
+         setLoading(false)
+    }
+
+    return{
+        user,
+        loading,
+        handleLogin,
+        handleRegister,
+        handleLogout
+    }
 }
