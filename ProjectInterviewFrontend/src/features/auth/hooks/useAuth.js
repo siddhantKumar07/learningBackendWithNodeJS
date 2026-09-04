3d4d5162f684a15c7613a4a707aa947ec7e204b4
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-04 10:39:14 +0530

    using all the api functions

--- ProjectInterviewFrontend/src/features/auth/hooks/useAuth.js
+++ ProjectInterviewFrontend/src/features/auth/hooks/useAuth.js
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import AuthProvider from "../Auth.Context.jsx";
-
-const context = useContext(AuthProvider)
+import { RegisterApi,LoginApi,LogoutApi,GetProfile } from "../services/auth.api.js";
 
 export const useAuth=()=>{
+const context = useContext(AuthProvider)
     const {user,setUser,loading,setLoading} = context;
+    
 }
