2ae68ca2ecf3d9054f9bfb2a4787cc16dc264f16
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-07 20:14:45 +0530

    using a pdf-parse package to extract the text from the pdf

--- ProjectInterviewBackend/src/controllers/interviewController.js
+++ ProjectInterviewBackend/src/controllers/interviewController.js
@@ -1,4 +1,20 @@
+const {generateInterviewReport} = require("../services/ai.service")
+const pdfParse = require("pdf-parse")
+const interviewReportModel = require("../models/interviewReport.model")
 const generateInterviewReportController = async(req,res)=>{
+const {selfDescription,jobDescription} = req.body;
 
+try{
+    const resume =await pdfParse( req.file.buffer.toString("base64"))
+
+    const report = await generateInterviewReport({
+        resume:resume,
+        selfDescription,
+        jobDescription
+    })
+    const savedReport =await interviewReportModel.create()
+}catch(error){
+    return res.status(500).json({message:"Internal server error",error:error.message});
+}
 }
 module.exports = {generateInterviewReportController}
