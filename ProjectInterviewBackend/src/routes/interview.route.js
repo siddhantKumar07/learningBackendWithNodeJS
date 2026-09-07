d41f301cdf962720d0c4c17f8b2ccd2280f1b22c
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-07 19:37:23 +0530

    using a pdf-parse package to extract the text from the pdf

--- ProjectInterviewBackend/src/routes/interview.route.js
+++ ProjectInterviewBackend/src/routes/interview.route.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const { authMiddleware } = require("../middleware/authMiddleware");
 const { generateInterviewReportController } = require("../controllers/interviewController");
-
+const multer =require("multer")
 const interviewRouter = express.Router();
 
+
+// Set up multer for file uploads
+const upload = multer({
+    storage:multer.memoryStorage(),
+    limits:{
+        fileSize:5*1024*1024 // 5mb
+    },
+})
 /**
  * @route POST /api/interview
  * @description This route is used to generate an interview report based on the provided resume, self-description, and job description. It uses the AI service to generate the report and returns it in the response.
  * @access private
  */
-interviewRouter.post("/",authMiddleware,generateInterviewReportController)
+interviewRouter.post("/",authMiddleware,upload.single("resume"),generateInterviewReportController)
 
 
 
