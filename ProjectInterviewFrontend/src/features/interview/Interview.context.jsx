6104b42dc4de916421c0ba67beaa27c7ea99b9c4
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-12 22:27:24 +0530

    adding the context api

--- ProjectInterviewFrontend/src/features/interview/Interview.context.jsx
+++ ProjectInterviewFrontend/src/features/interview/Interview.context.jsx
@@ -1,9 +1,15 @@
-import { Children, createContext } from "react";
+import { Children, createContext, useState } from "react";
 
 
 const interviewContextProvider = createContext()
 
 const interviewProvider = ({Children})=>{
-    
+    const [data, Setdata] = useState({})
+    const [loading, Setloading] = useState(true)
+    return(
+        <interviewContextProvider.Provider value={{data,Setdata,loading,Setloading}}>
+            {Children}
+        </interviewContextProvider.Provider>
+    )
 }
 
