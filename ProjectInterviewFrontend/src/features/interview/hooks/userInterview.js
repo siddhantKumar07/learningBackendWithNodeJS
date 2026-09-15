55375a0b51554d6bc381af5ac3caddc11521427a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-15 12:02:27 +0530

    adding the hooks to handle

--- ProjectInterviewFrontend/src/features/interview/hooks/userInterview.js
+++ ProjectInterviewFrontend/src/features/interview/hooks/userInterview.js
@@ -1,5 +1,27 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import {generateInterviewReport,getInterviewReportById,getAllInterviewReportsOfLoggedInUser} from '../services/interview.api'
-const hooks = ()=>{
-const []
+import InterviewProvider  from '../Interview.context'
+const useInterview = ()=>{
+const context = useContext(InterviewProvider)
+if(!context ){
+    throw new Error("useInterview must be used within an InterviewProvider")
+}
+const {report,Setreport,loading,Setloading,reports,Setreports} = context;
+
+const generateReport=async({
+      resume,
+  selfDescription,
+  jobDescription,
+
+})=>{
+    try{
+     Setloading(true)
+     const response = await generateInterviewReport({resume,selfDescription,jobDescription});
+     await Setreport(response)
+        Setloading(false)
+    }catch(error){
+        throw new Error("something went wrong",error.message)
+    }
+
+}
 }
