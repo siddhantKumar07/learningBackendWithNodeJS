c66bbae1f0ee852aad9c92847559d92cbbb5826e
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-15 14:54:30 +0530

    creating the functions which handle the comtext and then store the data

--- ProjectInterviewFrontend/src/features/interview/hooks/userInterview.js
+++ ProjectInterviewFrontend/src/features/interview/hooks/userInterview.js
@@ -1,58 +1,70 @@
-import { useContext, useState } from "react"
-import {generateInterviewReport,getInterviewReportById,getAllInterviewReportsOfLoggedInUser} from '../services/interview.api'
-import InterviewProvider  from '../Interview.context'
-const useInterview = ()=>{
-const context = useContext(InterviewProvider)
-if(!context ){
-    throw new Error("useInterview must be used within an InterviewProvider")
-}
-const {report,Setreport,loading,Setloading,reports,Setreports} = context;
+import { useContext, useState } from "react";
+import {
+  generateInterviewReport,
+  getInterviewReportById,
+  getAllInterviewReportsOfLoggedInUser,
+} from "../services/interview.api";
+import InterviewProvider from "../Interview.context";
+const useInterview = () => {
+  const context = useContext(InterviewProvider);
+  if (!context) {
+    throw new Error("useInterview must be used within an InterviewProvider");
+  }
+  const { report, Setreport, loading, Setloading, reports, Setreports } =
+    context;
 
-const generateReport=async({
-      resume,
-  selfDescription,
-  jobDescription,
-
-})=>{
-    try{
-     Setloading(true)
-     const response = await generateInterviewReport({resume,selfDescription,jobDescription});
-     await Setreport(response)
-        Setloading(false)
-    }catch(error){
-        Setloading(true)
-        throw new Error("something went wrong",error.message)
+  const generateReport = async ({
+    resume,
+    selfDescription,
+    jobDescription,
+  }) => {
+    try {
+      Setloading(true);
+      const response = await generateInterviewReport({
+        resume,
+        selfDescription,
+        jobDescription,
+      });
+      await Setreport(response.report);
+      Setloading(false);
+    } catch (error) {
+      Setloading(true);
+      throw new Error("something went wrong", error.message);
+    } finally {
+      Setloading(false);
     }
+  };
 
-}
-
-const getReportById=async(interviewId)=>{
-    try{
-        Setloading(true)
-     const response = await getInterviewReportById(interviewId);
-     await Setreport(response)
-     Setloading(false)
-    }catch(error){
-        Setloading(true)
-        throw new Error("something wrong in get Report by id",error.message)
+  const getReportById = async (interviewId) => {
+    try {
+      Setloading(true);
+      const response = await getInterviewReportById(interviewId);
+      await Setreport(response.report);
+      Setloading(false);
+    } catch (error) {
+      Setloading(true);
+      throw new Error("something wrong in get Report by id", error.message);
+    } finally {
+      Setloading(false);
     }
-}
+  };
 
-const getAllLoggedinReport= async ()=>{
-    try{
-  Setloading(true)
-  const response = await getAllInterviewReportsOfLoggedInUser();
-  await Setreports(response) 
-  Setloading(false)
-    }catch(error){
-        Setloading(true)
-        throw new Error("error in getAllLoggedinReport ",error.message)
+  const getAllLoggedinReport = async () => {
+    try {
+      Setloading(true);
+      const response = await getAllInterviewReportsOfLoggedInUser();
+      await Setreports(response.reports);
+      Setloading(false);
+    } catch (error) {
+      Setloading(true);
+      throw new Error("error in getAllLoggedinReport ", error.message);
+    } finally {
+      Setloading(false);
     }
-}
-return{
+  };
+  return {
     getAllLoggedinReport,
     getReportById,
-    generateReport
-
-}
-}
+    generateReport,
+  };
+};
